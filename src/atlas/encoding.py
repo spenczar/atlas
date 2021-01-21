8db3dae59c60434ae1de8d3580e89e2fbf8c4a56
Spencer Nelson <swnelson@uw.edu>
2021-01-21 15:02:26 -0800

    Implement uint64 packing

--- src/atlas/encoding.py
+++ src/atlas/encoding.py
@@ -2,5 +2,16 @@ from typing import List
 import struct
 
 
+def pack_uint64(data: int) -> bytes:
+    return struct.pack(">Q", data)
+
+
+def pack_unit64_list(data: List[int]) -> bytes:
+    result = b""
+    for i in data:
+        result += pack_uint64(i)
+    return result
+
+
 def unpack_uint64s(data: bytes) -> List[int]:
     return list(x[0] for x in struct.iter_unpack(">Q", data))
--- test/test_encoding.py
+++ test/test_encoding.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import atlas.encoding
+import pytest
 
 
 class TestUint64Packing:
@@ -9,26 +10,47 @@ class TestUint64Packing:
         have = atlas.encoding.unpack_uint64s(byteinput)
         assert have == expected
 
+    def _test_pack(self, val: int, expected: str):
+        have = atlas.encoding.pack_uint64(val)
+        assert have == bytearray.fromhex(expected)
+
+    def test_pack_zero(self):
+        self._test_pack(0, "0000 0000 0000 0000")
+
+    def test_pack_max(self):
+        self._test_pack((1 << 64) - 1, "ffff ffff ffff ffff")
+
+    def test_pack_overflow(self):
+        with pytest.raises(Exception):
+            atlas.encoding.pack_uint64(1 << 64)
+
     def test_unpack_empty(self):
         self._test_unpacking("", [])
 
     def test_unpack_one(self):
-        self._test_unpacking("0000000000000000", [0])
+        self._test_unpacking("0000 0000 0000 0000", [0])
 
     def test_unpack_several(self):
         self._test_unpacking(
-            "0000000000000001000000000000000200000000000000030000000000000004",
+            "0000 0000 0000 0001"
+            + "0000 0000 0000 0002"
+            + "0000 0000 0000 0003"
+            + "0000 0000 0000 0004",
             [1, 2, 3, 4],
         )
 
     def test_unpack_large_several(self):
         self._test_unpacking(
-            "00000001000000001000000000000000ffffffffffffffff",
+            "0000 0001 0000 0000" + "1000 0000 0000 0000" + "ffff ffff ffff ffff",
             [1 << 32, 1 << 60, (1 << 64) - 1],
         )
 
     def test_unpack_single_large(self):
         self._test_unpacking(
-            "ffffffffffffffff",
+            "ffff ffff ffff ffff",
             [(1 << 64) - 1],
         )
+
+    def test_unpack_incomplete_input(self):
+        with pytest.raises(Exception):
+            atlas.encoding.unpack_uint64s("1")
